01da4833301f6046f6e43e61f73f2ee2c8e9efc1
nan0 <nan0@nan0-Zenbook>
2012-11-25 18:19:59 +0000

    Commit website

---
+++ MobileNatiFood/js/feed_news.js
@@ -0,0 +1,27 @@
+function listPosts(data) {
+    var output = '';
+    $.each(data.posts,function(key,val) {
+        
+        var tempDiv = document.createElement("tempDiv");
+        tempDiv.innerHTML = val.excerpt;
+        $("a",tempDiv).remove();
+        var excerpt = tempDiv.innerHTML;
+                
+        output +='<div data-role="collapsible">';
+        output+='<h3>' + val.title + '</h3>';
+        output+='<p><a href="#blogpost" onclick="showPost(' + val.id + ')">';
+        output += excerpt + '</a></p>';
+        output+='</div>';
+    }); // go through each post
+	
+    $('#newslist').html(output);
+} // lists all the posts
+
+function showPost(id) {
+    $.getJSON('http://nplancher.info/natifood/blog?json=get_post&post_id=' + id + '&callback=?', function(data) {
+        var output='';
+        output += '<h3>' + data.post.title + '</h3>';
+        output += data.post.content;
+        $('#mypost').html(output);
+    }); //get JSON Data for Stories
+} //showPost	
