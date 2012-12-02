53a590966c4491966becea2d1ed3b4a75c328c93
nan0 <nan0@nan0-Zenbook>
2012-12-02 18:11:07 +0000

    Map Add (part 1)

--- MobileNatiFood/js/maps.js
+++ MobileNatiFood/js/maps.js
@@ -5,62 +5,76 @@ var map;
 
 function initialize(){
 
-//---Properties...
- directionsDisplay = new google.maps.DirectionsRenderer();
-  var mapOptions = {
-    zoom:10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    center: myCenter
-  }
-  map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
-  directionsDisplay.setMap(map);
-  directionsDisplay.setPanel(document.getElementById('direction_panel'));
+    //---Properties...
+    directionsDisplay = new google.maps.DirectionsRenderer();
+    var mapOptions = {
+        zoom:10,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: myCenter
+    }
+    map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+    directionsDisplay.setMap(map);
+    directionsDisplay.setPanel(document.getElementById('direction_panel'));
 
-//--------Marker...
+    //--------Marker...
 
-var marker=new google.maps.Marker({
-  position:myCenter
-  });
-marker.setMap(map);
+    var marker=new google.maps.Marker({
+        position:myCenter
+    });
+    marker.setMap(map);
 
-//--------Marker Zoom on click...
+    //--------Marker Zoom on click...
 
-google.maps.event.addListener(marker,'click',function() {
-  map.setZoom(15);
-  map.setCenter(marker.getPosition());
-  });
+    google.maps.event.addListener(marker,'click',function() {
+        map.setZoom(15);
+        map.setCenter(marker.getPosition());
+    });
   
-  //--------Info Window...
+    //--------Info Window...
 
-var infowindow = new google.maps.InfoWindow({
-  content:'<img style="float : left; width = 20px; " width="100px" src="images/logo.png" alt="NatiFood !"/><p>Natifood main office is gracefully hosted in the institute of technology of Blanchardstown !</p>'
-  });
-infowindow.open(map,marker);
- }
+    var infowindow = new google.maps.InfoWindow({
+        content:'<img style="float : left; width = 20px; " width="100px" src="images/logo.png" alt="NatiFood !"/><p>Natifood main office is gracefully hosted in the institute of technology of Blanchardstown !</p>'
+    });
+    infowindow.open(map,marker);
+    
+    $('#button_directions').hide();
+}
 
- //----- compute directions
+//----- compute directions
 function calcRoute() {
- 
-  var start = prompt("Please enter your address :");
 
-  var end = myCenter;
-  var request = {
-    origin:start,
-    destination:end,
-    travelMode: google.maps.TravelMode.DRIVING
-  };
+    var end = myCenter;
+    var request = {
+        origin:$('#input_directions').val(),
+        destination:end,
+        travelMode: google.maps.TravelMode.DRIVING
+    };
   
-  directionsService.route(request, function(result, status) {
+    directionsService.route(request, function(result, status) {
 
-		if (status == google.maps.DirectionsStatus.OK) {
-			directionsDisplay.setDirections(result);
-			$('#direction_panel').hide();
-			$('#link_direction_instruction').fadeIn();
-		}
-		else{
-			alert("Address not found, please be precisier");
-		}
-  });
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(result);
+            $('#direction_panel').hide();
+            $('#link_direction_instruction').fadeIn();
+            $('#button_directions').show();
+        }
+        else{
+            alert("Address not found, please be precisier");
+        }
+    });
+}
+
+
+
+function showDirections(){
+    if ($('#direction_panel').css("display") != "none"){
+        $('#direction_panel').css("display","none");
+        $('#button_directions p').text("Show directions");
+    }
+    else{
+        $('#direction_panel').css("display","block"); 
+        $('#button_directions p').text("Hide directions");
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
